8675fe145a7ef4b5bc26155d3b4b53a2f5b59883
solar05 <solar7455@gmail.com>
2018-10-25 00:39:26 +0300

    Small refactoring of Slot

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -8,7 +8,7 @@ public class Slot {
     public static void main(String... __) {
         int initialAccount = 100;
         int betAmount = 10;
-        int winAccount = 10000;
+        int winAccount = 1_000;
         int randNum = 7;
         int firstRoll = 0;
         int secondRoll = 0;
@@ -17,15 +17,15 @@ public class Slot {
             firstRoll = (firstRoll + (int) round(random() * 100)) % randNum;
             secondRoll = (secondRoll + (int) round(random() * 100)) % randNum;
             thirdRoll = (thirdRoll + (int) round(random() * 100)) % randNum;
-            System.out.println(String.format("У вас %d, ставка - %d", initialAccount, betAmount));
-            System.out.println(String.format("Крутим барабаны! Розыгрыш принёс следующие результаты:\n" +
-                    "первый барабан - %d, второй - %d, третий - %d", firstRoll, secondRoll, thirdRoll));
+            System.out.format("У вас %d, ставка - %d\n", initialAccount, betAmount);
+            System.out.format("Крутим барабаны! Розыгрыш принёс следующие результаты:\n" +
+                    "первый барабан - %d, второй - %d, третий - %d\n", firstRoll, secondRoll, thirdRoll);
             if ((firstRoll == secondRoll) && (secondRoll == thirdRoll)) {
                 initialAccount += winAccount;
-                System.out.println(String.format("Выигрыш %d, ваш капитал теперь составляет: %d", winAccount, initialAccount));
+                System.out.format("Выигрыш %d, ваш капитал теперь составляет: %d\n", winAccount, initialAccount);
             } else {
                 initialAccount -= betAmount;
-                System.out.println(String.format("Проигрыш %d, ваш капитал теперь составляет: %d", betAmount, initialAccount));
+                System.out.format("Проигрыш %d, ваш капитал теперь составляет: %d\n", betAmount, initialAccount);
             }
         } while (initialAccount != 0);
     }
